99d9faa32a29a879708bcbe75acc4d63d65092ec
Dia-Garg <darjdiagarg@gmail.com>
2026-02-23 16:02:45 +0530

    UC1: Add welcome message module

--- Palindrome_Checker/src/PalindromeCheckerApp.java
+++ Palindrome_Checker/src/PalindromeCheckerApp.java
@@ -2,36 +2,12 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    // Method to check palindrome
-    public static boolean isPalindrome(String input) {
-        // Remove non-alphanumeric characters and convert to lowercase
-        String cleaned = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        int left = 0;
-        int right = cleaned.length() - 1;
-
-        while (left < right) {
-            if (cleaned.charAt(left) != cleaned.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a string:");
-        String input = sc.nextLine();
-
-        if (isPalindrome(input)) {
-            System.out.println("The given string is a Palindrome.");
-        } else {
-            System.out.println("The given string is NOT a Palindrome.");
-        }
-
+        System.out.println("Welcome to the Palindrome Checker Management System.\nVersion : 1.0\nSystem initialized successfully.");
         sc.close();
     }
 }
